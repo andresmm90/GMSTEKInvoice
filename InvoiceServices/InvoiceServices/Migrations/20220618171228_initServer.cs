b1daede388cab6bfc559c8f104d9a0e9f7721b75
andresmm90 <andresmm.0490@gmail.com>
2022-06-18 12:20:18 -0500

    Despliegue de webApi en servidor Azure

---
+++ InvoiceServices/InvoiceServices/Migrations/20220618171228_initServer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceServices.Migrations
+{
+    public partial class initServer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
